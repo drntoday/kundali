70dbc50828e3188fdebaa5cbf6659f01b18aceb7
drntoday <drn.today@outlook.com>
2026-01-15 18:00:28 +0530

    Create nakshatra.js

---
+++ zodiac/nakshatra.js
@@ -0,0 +1,13 @@
+export function getNakshatra(deg){
+  const naks = [
+    "Ashwini","Bharani","Krittika","Rohini","Mrigashirsha","Ardra",
+    "Punarvasu","Pushya","Ashlesha","Magha","Purva Phalguni","Uttara Phalguni",
+    "Hasta","Chitra","Swati","Vishakha","Anuradha","Jyeshtha",
+    "Mula","Purva Ashadha","Uttara Ashadha","Shravana","Dhanishta",
+    "Shatabhisha","Purva Bhadrapada","Uttara Bhadrapada","Revati"
+  ];
+
+  const size = 13.333333;
+  const index = Math.floor(deg / size);
+  return naks[index];
+}
