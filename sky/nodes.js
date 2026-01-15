00c9409049051dc31c713fb627f5c94b6cb9cdb0
drntoday <drn.today@outlook.com>
2026-01-15 19:36:20 +0530

    Update nodes.js

--- sky/nodes.js
+++ sky/nodes.js
@@ -1,5 +1,10 @@
+function norm(x){
+  return (x % 360 + 360) % 360;
+}
+
 export function rahuKetu(d){
-  const rahu = (125.04452 - 0.0529538083 * d) % 360;
-  const ketu = (rahu + 180) % 360;
+  const rahu = norm(125.04452 - 0.0529538083 * d);
+  const ketu = norm(rahu + 180);
   return { rahu, ketu };
 }
+
