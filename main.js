282c110ddd7396f1c3adca077357c53e5fb78887
drntoday <drn.today@outlook.com>
2026-01-15 18:12:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { getRashi } from "./zodiac/rashi.js";
 import { getNakshatra } from "./zodiac/nakshatra.js";
 import { trueSolarTime } from "./time/trueSolarTime.js";
 import { calculateLagna } from "./time/lagna.js";
+import { getMahaDasha } from "./dasha/vimshottari.js";
 
 async function start(){
   const date = document.getElementById("date").value;
@@ -32,10 +33,13 @@ async function start(){
 
   const tsm = trueSolarTime(dt, lon, g.results[0].timezone);
   const lagnaDeg = calculateLagna(tsm);
+  const dasha = getMahaDasha(moonSid);
 
   document.getElementById("output").textContent =
     "Sun: " + sunSid.toFixed(2) + "° " + sunRashi + "\n" +
     "Moon: " + moonSid.toFixed(2) + "° " + moonRashi + "\n" +
     "Moon Nakshatra: " + moonNak + "\n" +
+    "Maha Dasha Lord: " + dasha.lord + "\n" +
+    "Remaining Years: " + dasha.remainingYears + "\n" +
     "Lagna: " + lagnaDeg.toFixed(2);
 }
