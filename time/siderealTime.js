8776407571ddda39edc4b1e6ac91451acf20b513
drntoday <drn.today@outlook.com>
2026-01-15 20:52:53 +0530

    Update siderealTime.js

--- time/siderealTime.js
+++ time/siderealTime.js
@@ -1,3 +1,14 @@
-export function localSiderealTime(trueSolarMinutes){
-  return (trueSolarMinutes * 1.0027379) % 1440;
+export function julianDay(date){
+  return date / 86400000 + 2440587.5;
+}
+
+export function localSiderealTime(jd, longitude){
+  const T = (jd - 2451545.0) / 36525;
+  let theta = 280.46061837
+    + 360.98564736629 * (jd - 2451545)
+    + 0.000387933 * T*T
+    - T*T*T / 38710000;
+
+  theta = (theta + longitude) % 360;
+  return theta;
 }
