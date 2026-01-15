f33625ae6942d93c90b0635f6282b7735ff2e5e3
drntoday <drn.today@outlook.com>
2026-01-15 14:41:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
+import { trueSolarTime } from "./time/trueSolarTime.js";
+import { calculateLagna } from "./time/lagna.js";
+
 async function start(){
   const date = document.getElementById("date").value;
   const time = document.getElementById("time").value;
   const place = document.getElementById("place").value;
 
-  if(!date || !time || !place){
-    alert("Fill all fields");
-    return;
-  }
-
   const geo = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${place}`);
   const g = await geo.json();
-
-  if(!g.results){
-    alert("Place not found");
-    return;
-  }
 
   const lat = g.results[0].latitude;
   const lon = g.results[0].longitude;
   const tz  = g.results[0].timezone;
 
+  const dt = new Date(date + "T" + time);
+
+  const tsm = trueSolarTime(dt, lon, tz);
+  const lagna = calculateLagna(tsm);
+
   document.getElementById("output").textContent =
     "Latitude: " + lat + "\n" +
     "Longitude: " + lon + "\n" +
-    "Timezone: " + tz;
+    "True Solar Minutes: " + tsm.toFixed(2) + "\n" +
+    "Lagna Degree: " + lagna.toFixed(2);
 }
